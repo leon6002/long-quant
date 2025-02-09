c18368e6f08be824bfd06256956eb0c833ea2a32
gulong <gulong.chen@hotmail.com>
2025-02-09 19:37:00 +0800

    修改日志格式

--- src/config/base.py
+++ src/config/base.py
@@ -14,7 +14,7 @@ MONGODB_NAME= os.getenv("MONGODB_NAME")
 # 集中配置日志
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)s: %(message)s',
+    format='%(asctime)s %(levelname)s [%(filename)s]: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
