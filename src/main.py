089c8c408cd5ba56906e25c056dc372acef4c584
gulong <gulong.chen@hotmail.com>
2025-04-09 22:24:45 +0800

    handle exception

--- src/main.py
+++ src/main.py
@@ -5,18 +5,21 @@ from core import main
 
 def job():
     logger.info("Scheduled job started")
-    main()
+    try:
+        main()
+    except Exception as e:
+        logger.error("unknown exception", e)
     logger.info("Scheduled job completed")
 
 def start_job():
     # 立即运行一次任务
     job()
     # 每2分钟执行一次任务
-    schedule.every(2).minutes.do(job)
+    schedule.every(10).minutes.do(job)
 
     while True:
         schedule.run_pending()
-        time.sleep(5)
+        time.sleep(10)
 
 if __name__ == "__main__":
     start_job()
